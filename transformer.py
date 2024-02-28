1c47f5368164d2f70d6852f4140357ed52f395bc
tha-santacruz <nicola.santacroce@protonmail.com>
2024-02-28 18:06:44 +0100

    added embeddings

--- transformer.py
+++ transformer.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from dataset import OpusTranslationDataset
 
 class MLP(nn.Module):
     def __init__(self, size_in, size_hidden, size_out):
@@ -86,38 +89,69 @@ class MaskedMultiHeadAttention(nn.Module):
         y = self.linear(y)
         return y
     
+
 class EncoderLayer(nn.Module):
     def __init__(self, d_model, h):
         super(EncoderLayer, self).__init__()
         self.masked_multi_head_attention = MaskedMultiHeadAttention(d_model, h)
         self.feed_forward = MLP(d_model, int(d_model*4), d_model)
+        self.dropout = nn.Dropout(p=0.1)
         self.layer_norm = nn.LayerNorm(d_model)
 
     def forward(self, x, m):
-        y = self.masked_multi_head_attention(x, x, x, m) + x
-        y = self.layer_norm(y)
-        y = self.feed_forward(y) + y
-        y = self.layer_norm(y)
-        return y
+        y0 = self.masked_multi_head_attention(x, x, x, m)
+        y1 = self.dropout(y0) + x
+        y2 = self.layer_norm(y1)
+        y3 = self.feed_forward(y2)
+        y4 = self.dropout(y3) + y2
+        y5 = self.layer_norm(y4)
+        return y5
     
+
 class DecoderLayer(nn.Module):
     def __init__(self, d_model, h):
         super(DecoderLayer, self).__init__()
         self.masked_multi_head_attention = MaskedMultiHeadAttention(d_model, h)
         self.multi_head_attention = MultiHeadAttention(d_model, h)
         self.feed_forward = MLP(d_model, int(d_model*4), d_model)
+        self.dropout = nn.Dropout(p=0.1)
         self.layer_norm = nn.LayerNorm(d_model)
 
-    def forward(self, x_1, x_2, m):
-        y = self.masked_multi_head_attention(x_1, x_1, x_1, m) + x_1
-        y = self.layer_norm(y)
-        y = self.multi_head_attention(y, x_2, x_2) + y
-        y = self.layer_norm(y)
-        y = self.feed_forward(y) + y
-        y = self.layer_norm(y)
-        return y
+    def forward(self, x0, x1, m):
+        y0 = self.masked_multi_head_attention(x0, x0, x0, m)
+        y1 = self.dropout(y0) + x0
+        y2 = self.layer_norm(y1)
+        y3 = self.multi_head_attention(y2, x1, x1)
+        y4 = self.dropout(y3) + y2
+        y5 = self.layer_norm(y4)
+        y6 = self.feed_forward(y5)
+        y7 = self.dropout(y6) + y5
+        y8 = self.layer_norm(y7)
+        return y8
     
-# Batches of identical size only
+
+class EmbeddingLayer(nn.Module):
+    def __init__(self, d_model, num_tokens):
+        super(EmbeddingLayer, self).__init__()
+        #PE(pos, 2i) = sin(pos/10000**(2i/d_model))
+        #PE(pos, 2i+1) = cos(pos/10000**(2i/d_model))
+        self.token_embeddings = nn.Parameter(torch.randn(num_tokens, d_model), requires_grad=True).mul(d_model**0.5)
+        self.sine = lambda pos, j : torch.sin(pos.div(torch.tensor(10000).pow(j/d_model)))
+        self.cosine = lambda pos, j : torch.cos(pos.div(torch.tensor(10000).pow(j/d_model)))
+        self.dropout = nn.Dropout(p=0.1)
+
+    def forward(self, x):
+        y1 = self.token_embeddings[x]
+        y2 = torch.zeros_like(y1)
+        pos = torch.arange(y1.size(1))
+        for i in range(int(y1.size(2)/2)):
+            y2[:, :, 2*i] = self.sine(pos, 2*i)
+            y2[:, :, 2*i+1] = self.cosine(pos, 2*i)
+
+        y3 = self.dropout(y1.add(y2))
+        return y3
+    
+
 class TransformerModel(nn.Module):
     def __init__(self, d_model, h, l, num_tokens):
         super(TransformerModel, self).__init__()
@@ -127,102 +161,62 @@ class TransformerModel(nn.Module):
         self.decoder_layers = nn.ModuleList(
             [DecoderLayer(d_model, h) for i in range(l)]
         )
-        self.embeddings = torch.randn(num_tokens, d_model)
+        self.embedding_layer = EmbeddingLayer(d_model, num_tokens)
         self.linear = nn.Linear(in_features=d_model, out_features=num_tokens)
         self.activation = nn.Softmax(dim=-1)
     
-    def forward(self, input_tokens, output_tokens=None):
-        # embedding
-        input_seq = self.embeddings[input_tokens]
-        if output_tokens is not None:
-            mask = torch.zeros_like(input_tokens)
-            mask[:, :output_tokens.size()[-1]] = 1
-            i = (1-mask).flatten().nonzero()[0]
-
-            output_seq = self.embeddings[output_tokens]
-            temp = torch.zeros_like(input_seq)
-            temp[:output_seq.size()[0], :output_seq.size()[1]] = output_seq
-            output_seq = temp
-
-            temp = torch.zeros_like(input_tokens)
-            temp[:output_tokens.size()[0], :output_tokens.size()[1]] = output_tokens
-            output_tokens = temp
-
-        else:
-            output_seq = torch.randn_like(input_seq)
-            output_tokens = torch.empty_like(input_tokens)
-
-        """
-        if output_tokens is not None:
-            output_seq = self.embeddings[output_seq]
-            mask = torch.zeros(input_seq.size()[:-1])
-            mask[:, :output_seq.size()[-2]] = 1
-            temp = torch.zeros_like(input_seq)
-            temp[:output_seq.size()[0], :output_seq.size()[1]] = output_seq
-            output_seq = temp
-            i = (1-mask).flatten().nonzero()[0]
-        else:
-            output_seq = torch.randn_like(input_seq)"""
-
-        print(input_seq.size())
-        print(output_seq.size())
-        print(output_seq[:, :, 0])
-        print(output_tokens)
-        print(mask)
-        print(i)
-
-        """
-        input = input_seq
-        output = output_seq
-        
-        if not output:
-            output = torch.randn_like(input)
-        if not mask:
-            mask = torch.zeros(input.size()[:-1])
-            i = 0
-        else:
-            i = mask.flatten().nonzero()[0]
-
-
+    def forward(self, input_ids, input_mask):
+        input_seq = self.embedding_layer(input_ids)
+        output_ids = torch.zeros_like(input_ids)
+        output_seq = torch.zeros_like(input_seq)
+        output_mask = torch.zeros_like(input_mask)
+        output_probs = torch.zeros((input_seq.size(0), input_seq.size(1), self.linear.out_features))
         encoder_hidden_states = []
         for encoder_layer in self.encoder_layers:
-            input = encoder_layer(input)
-            encoder_hidden_states.append(input)
+            input_seq = encoder_layer(input_seq, input_mask)
+            encoder_hidden_states.append(input_seq)
 
-        for j, decoder_layer in enumerate(self.decoder_layers):
-            print(output.size())
-            output = decoder_layer(
-                output,
-                encoder_hidden_states[j],
-                mask
-            )
-        output = self.linear(output)
-        probabilities = self.activation(output)
-        print(probabilities.size())
-        return probabilities"""
+        from tqdm import trange        
+        for i in trange(output_ids.size(1)):
+            for j, decoder_layer in enumerate(self.decoder_layers):
+                output_seq = decoder_layer(
+                    output_seq,
+                    encoder_hidden_states[j],
+                    output_mask
+                )
+            logits = self.linear(output_seq)
+            probs = self.activation(logits)
+            output_probs[:, i, :] = probs[:, i, :]
+            output_ids[:, i] = probs[:, i, :].argmax(dim=-1)
+            output_seq = self.embedding_layer(output_ids)
+            output_mask[:, i] = 1
+
+        return output_ids, output_probs
 
     
 if __name__ == "__main__":
+
+    dataset = OpusTranslationDataset(
+        dataset_name="WikiMatrix",
+        language_source="fr",
+        language_target="it"
+    )
+    dataset.use_set("train")
+    dataloader = DataLoader(dataset, batch_size=4)
+    batch = next(iter(dataloader))
+    net = TransformerModel(
+        d_model=512, 
+        h=8, 
+        l=6, 
+        num_tokens=dataset.vocab_size)
     
-    """a = torch.randn(4, 32, 512)
-    enc = EncoderLayer(512, 8)
-    m = torch.zeros(4, 32)
-    m[:, :12] = 1
-    b = enc(a, m)""" 
-    """
-    print(b.shape)
-    dec = DecoderLayer(512, 8)
-    mask = torch.zeros(a.size(0), a.size(1))
-    mask[:, :10] = 1
-    c = dec(a, b, mask)
-    print(c.size())"""
+    #with torch.no_grad():
+    #    ids, probs = net(batch[0], batch[1])
 
-    """#input = torch.randn(4, 32, 512)
-    model = TransformerModel(512, 8, 6, 30000)
-    #model(input)
-
-    input_seq = torch.randint(10, (4, 32))
-    output_seq = torch.randint(10, (4, 12))
-
-    model(input_seq, output_seq)"""
-
+    #print(dataset.tokenizer.decode(ids[0].tolist()))
+    
+    parameters_count = 0
+    for name, param in net.named_parameters():
+        if param.requires_grad:
+            parameters_count += param.numel()
+    print(f"number of parameters in the model : {parameters_count}")
