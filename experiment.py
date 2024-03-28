89f2cb440527e0e83ae6f4bb7ef713e0f55d0795
tha-santacruz <nicola.santacroce@protonmail.com>
2024-03-28 16:21:51 +0100

    added new arguments and gradients clipping

--- experiment.py
+++ experiment.py
@@ -12,21 +12,22 @@ class Experiment():
     def __init__(self, cfg):
         self.cfg = cfg
         self.dataset = OpusTranslationDataset(
-            dataset_name=self.cfg.DATASET,
+            dataset_name=self.cfg.DATASET_NAME,
             language_source=self.cfg.LANGUAGE_INPUT,
             language_target=self.cfg.LANGUAGE_TARGET,
-            sequence_length=self.cfg.SEQUENCE_LENGTH
+            sequence_length=self.cfg.SEQUENCE_LENGTH,
+            vocab_size=self.cfg.VOCAB_SIZE
         )
         self.net = TransformerModel(
-            d_model=512, 
-            h=8, 
-            l=6, 
-            num_tokens=self.dataset.vocab_size,
+            d_model=self.cfg.MODEL_DIMS, 
+            h=self.cfg.MODEL_HEADS, 
+            l=self.cfg.MODEL_LAYERS, 
+            num_tokens=self.cfg.VOCAB_SIZE,
         ).to(device=self.cfg.DEVICE)
         if self.cfg.LOAD_CHECKPOINT:
-            self.net.load_state_dict(torch.load(f"checkpoints/bestmodel_{self.cfg.DATASET}.pt"))
+            self.net.load_state_dict(torch.load(f"checkpoints/{self.cfg.LOAD_CHECKPOINT}.pt"))
         
-        self.default_dtype = torch.float16 if self.cfg.PARAM_BITS == 16 else torch.float32
+        self.default_dtype = torch.float16 if self.cfg.HALF_BITS else torch.float32
 
         for param in self.net.parameters():
             param.data = param.data.to(dtype=self.default_dtype).clone().detach()
@@ -57,7 +58,11 @@ class Experiment():
         round_acc = 0.0
         batch_num = 1
         pbar = tqdm(loader)
+
+
         for batch in pbar:
+            self.optimizer.zero_grad()
+
             input_ids = batch[0].to(dtype=torch.int64, device=self.cfg.DEVICE)
             input_mask = batch[1].to(dtype=self.default_dtype, device=self.cfg.DEVICE)
             target_ids = batch[2].to(dtype=torch.int64, device=self.cfg.DEVICE)
@@ -67,8 +72,10 @@ class Experiment():
             
             loss = self.criterion(pred_probs.view(-1, pred_probs.size(-1)), target_ids.view(-1))
 
-            self.optimizer.zero_grad()
             self.scaler.scale(loss).backward()
+
+            nn.utils.clip_grad_norm(self.net.parameters(), 1.0)
+
             self.scaler.step(self.optimizer)
             self.scaler.update()
             
@@ -97,13 +104,13 @@ class Experiment():
         batch_num = 1
         pbar = tqdm(loader)
         for batch in pbar:
-            input_ids = batch[0].to(device=self.cfg.DEVICE).int()
+            input_ids = batch[0].to(dtype=torch.int64, device=self.cfg.DEVICE)
             input_mask = batch[1].to(dtype=self.default_dtype, device=self.cfg.DEVICE)
-            target_ids = batch[2].to(dtype=self.default_dtype, device=self.cfg.DEVICE)
+            target_ids = batch[2].to(dtype=torch.int64, device=self.cfg.DEVICE)
             target_mask = batch[3].to(dtype=self.default_dtype, device=self.cfg.DEVICE)
             with torch.no_grad():
                 pred_probs, pred_ids = self.net(input_ids, input_mask)
-                loss = self.criterion(pred_probs, target_ids)
+                loss = self.criterion(pred_probs.view(-1, pred_probs.size(-1)), target_ids.view(-1))
                     
                 success = (pred_ids==target_ids)[target_mask==1]
                 acc = success.sum()/success.size(-1)
@@ -121,9 +128,10 @@ class Experiment():
         for epoch in range(self.cfg.EPOCH_NUM):
             train_loss, train_acc = self.train_round()
             val_loss, val_acc = self.eval_round()
-
+            
             if val_loss < best_val_loss:
-                torch.save(self.net.state_dict(), f"checkpoints/bestmodel_{self.cfg.DATASET}.pt")
+                if self.cfg.SAVE_CHECKPOINTS:
+                    torch.save(self.net.state_dict(), f"checkpoints/{self.cfg.SAVE_CHECKPOINTS}.pt")
                 best_val_loss = val_loss
 
             print(f"Metrics after epoch {epoch+1} : train loss : {train_loss:.4f} | train accuracy : {train_acc:.4f} | validation loss : {val_loss:.4f} | validation accuracy {val_acc:.4f}")
