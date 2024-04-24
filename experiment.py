d3406fce5ffc008a3c6897aaabf55b7909366b3e
tha-santacruz <nicola.santacroce@protonmail.com>
2024-04-24 06:36:09 +0200

    added testing loop

--- experiment.py
+++ experiment.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as f
 from tqdm import tqdm
 from torch.utils.data import DataLoader
-from torch.utils.checkpoint import checkpoint
 
 from dataset import OpusTranslationDataset
 from transformer import TransformerModel
@@ -46,7 +45,11 @@ class Experiment():
             betas=(0.9, 0.98),
             eps=1e-9,
         )
-        self.criterion = nn.CrossEntropyLoss(label_smoothing=0.1).to(device=self.cfg.DEVICE)
+        
+        token_weights = torch.ones(self.cfg.VOCAB_SIZE)
+        token_weights[1] = 0 # PAD token
+        token_weights[3] = 1/10000 # EOS token
+        self.criterion = nn.CrossEntropyLoss(weight=token_weights, label_smoothing=0.1, reduction="mean").to(device=self.cfg.DEVICE)
         self.scaler = torch.cuda.amp.GradScaler()
 
     def train_round(self):
@@ -61,7 +64,7 @@ class Experiment():
         self.net.train()
         round_loss = 0.0
         round_acc = 0.0
-        batch_num = 1
+        batch_num = 0
         pbar = tqdm(loader)
 
 
@@ -74,15 +77,21 @@ class Experiment():
             target_mask = batch[3].to(dtype=self.default_dtype)
 
             with torch.cuda.amp.autocast():
-                pred_probs, pred_ids = self.net(input_ids, input_mask)
+                pred_probs, pred_ids = probs, ids = self.net(
+                    input_ids, 
+                    input_mask,
+                    target_ids,
+                    target_mask 
+                )
             
             loss = self.criterion(pred_probs.view(-1, pred_probs.size(-1)), target_ids.view(-1))
             self.scaler.scale(loss).backward()
+            self.scaler.unscale_(self.optimizer)
             nn.utils.clip_grad_norm(self.net.parameters(), 1.0)
 
             self.scaler.step(self.optimizer)
             self.scaler.update()
-            
+
             with torch.no_grad():
                 success = (pred_ids==target_ids)[target_mask==1]
                 acc = success.sum()/success.size(-1)
@@ -91,27 +100,29 @@ class Experiment():
             round_loss += loss.item()
             round_acc += acc.item()
             batch_num += 1
+
         return round_loss/batch_num, round_acc/batch_num
     
-    def eval_round(self):
-        self.dataset.use_set("val")
+    def eval_round(self, evaluatated_set="val"):
+        self.dataset.use_set(evaluatated_set)
         loader = DataLoader(
             dataset=self.dataset,
             batch_size=self.cfg.BATCH_SIZE,
-            shuffle=True,
+            shuffle=False,
             prefetch_factor=1,
             num_workers=self.cfg.NUM_WORKERS,
         )
         self.net.eval()
         round_loss = 0.0
         round_acc = 0.0
-        batch_num = 1
+        batch_num = 0
         pbar = tqdm(loader)
         for batch in pbar:
             input_ids = batch[0].to(dtype=torch.int64, device=self.cfg.DEVICE)
             input_mask = batch[1].to(dtype=self.default_dtype, device=self.cfg.DEVICE)
             target_ids = batch[2].to(dtype=torch.int64, device=self.cfg.DEVICE)
             target_mask = batch[3].to(dtype=self.default_dtype, device=self.cfg.DEVICE)
+
             with torch.no_grad():
                 pred_probs, pred_ids = self.net(input_ids, input_mask)
                 loss = self.criterion(pred_probs.view(-1, pred_probs.size(-1)), target_ids.view(-1))
@@ -123,6 +134,7 @@ class Experiment():
             round_loss += loss.item()
             round_acc += acc.item()
             batch_num += 1
+
         return round_loss/batch_num, round_acc/batch_num
 
     def train(self):
@@ -139,7 +151,13 @@ class Experiment():
                 best_val_loss = val_loss
 
             print(f"Metrics after epoch {epoch+1} : train loss : {train_loss:.4f} | train accuracy : {train_acc:.4f} | validation loss : {val_loss:.4f} | validation accuracy {val_acc:.4f}")
+
+    def test(self):
         
+        
+            test_loss, test_acc = self.eval_round(evaluatated_set="test")
+
+            print(f"Metrics at test time : test loss : {test_loss:.4f} | test accuracy : {test_acc:.4f}")        
 
         
     
